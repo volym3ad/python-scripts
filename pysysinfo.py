080755a0dbe2b42e41e9e249411a03bc766303b5
Vladyslav Voloshyn <kalym.share@gmail.com>
2016-04-28 15:17:45 +0300

    Added files via upload

---
+++ pysysinfo.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+#A System Information Gathering Script
+import subprocess
+
+#Command 1
+def uname_func():
+	uname = "uname"
+	uname_arg = "-a"
+	print "Gathering system information with %s command:\n" % uname
+	subprocess.call([uname, uname_arg])
+
+#Command 2
+def disk_func():
+	diskspace = "df"
+	diskspace_arg = "-h"
+	print "Gathering diskspace information %s command:\n" % diskspace
+	subprocess.call([diskspace, diskspace_arg])
+
+
+def main():
+	uname_func()
+	disk_func()
+
+if __name__ == "__main__"
+	main()
